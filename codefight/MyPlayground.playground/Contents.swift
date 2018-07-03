2fde61627aa5ed4e78ee1036f56977060cb53a7b
Martin Chibwe <progr3ss@Martins-MacBook-Pro.local>
2018-07-03 01:52:17 -0700

    adding files

--- codefight/MyPlayground.playground/Contents.swift
+++ codefight/MyPlayground.playground/Contents.swift
@@ -9,67 +9,22 @@ var matrix = [[1, 1, 1, 0],
               [0, 5, 0, 1],
               [2, 1, 3, 10]]
 
-//
-//matrix[0][1]
-//matrix[1][1]
-//matrix[2]
-//var colum = [Int]()
-matrix.count
-var row = matrix[0].count
-func matrixElementsSum(matrix: [[Int]]) -> Int {
-    var total = 0
-    for i in 0..<matrix[0].count {
-//        print(i)
-        for j in 0..<matrix.count {
-//            print(j)
-            let room = matrix[j][i]
-//            pritn(room)
-//            print(room)
-            guard room != 0  else {
-                
-                break
-                
-            }
-//            print(room)
-//            total += room
+
+var s1 = "aabcc"
+var s2 = "adcaa"
+
+func commonCharacterCount(s1: String, s2: String) -> Int {
+    var count = 0
+    for i in Set(s1){
+        if s2.contains(i) {
+            print(i)
+            count += 1
         }
     }
-    return total
+    
+    
+    return count
+    
 }
-matrix[0][0]
-print(matrixElementsSum(matrix: matrix))
-//func matrixElementsSum(matrix: [[Int]]) -> Int {
-//    var a = matrix
-//    for i in 0..<(a.count - 1) {
-//
-//        for j in 0..<a[i].count {
-//
-//            if a[i][j] < 1 {
-//                for k in (i+1)..<a.count {
-//                    a[k][j] = 0
-//                }
-//            }
-//        }
-//    }
-//
-//    var r = 0
-//    for i in 0..<a.count {
-//        for j in 0..<a[i].count {
-//            r += a[i][j]
-//        }
-//    }
-//    return r
-//}
-//print(matrixElementsSum(matrix: matrix))
-//for i in 0..<matrix.count{
-//
-//
-//    for j in 0...row-1 {
-//
-////        print(matrix[0])
-//
-//    }
-//
-//}
-//matrix[0][0]
 
+print(commonCharacterCount(s1: s1, s2: s2))
--- leetcode/MyPlayground.playground/Contents.swift
+++ leetcode/MyPlayground.playground/Contents.swift
@@ -1,22 +1,58 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
-
-var str = "Hello, playground"
-var input = [2, 6, 4, 8, 10, 9, 15]
-
-
-for i in 0..<input.count-1{
-        var count = 0
-
-        if i > i + 1 {
-            input.swapAt(i, i+1)
-            count += 1
-        }
-        print(count)
-}
-
-    
+//import UIKit
+import PlaygroundSupport
+//var str = "Hello, playground"
+//var input = [2, 6, 4, 8, 10, 9, 15]
+//
+//
+//for i in 0..<input.count-1{
+//        var count = 0
+//
+//        if i > i + 1 {
+//            input.swapAt(i, i+1)
+//            count += 1
+//        }
+//        print(count)
+//}
+//
+//
 
 //print(sortFun(input: &input))
+
+var a  = 123
+
+var b = String(a)
+
+
+func reverseInt(_ x: Int ) -> Int {
+    
+    if x == 0 {
+        return x
+    }
+    
+    var ret = 0
+    var input = x
+    
+    while input != 0 {
+        ret = ret*10 + ret%10
+        input =  input/10
+        
+    }
+    
+    return ret
+    
+}
+
+reverseInt(123)
+var  j = [1,2,3,4,5,6,7]
+func rotateArray(_ nums: [Int], _ k: Int)  -> [Int]{
+    var new = nums
+    for i in 0..<k {
+        new.insert(new.popLast()!, at: 0)
+    }
+    return new
+}
+print(rotateArray(j, 3))
+
 
